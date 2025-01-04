86ae041e4d060e9cf108ac71a69fa665aa197004
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2025-01-04 13:25:51 +0600

    environment setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+
+export  const router = createBrowserRouter(
+   [
+      {
+         path: '/',
+         element: <Home></Home>,
+         errorElement: <h1>404</h1>,
+         
+      }
+   ]
+)
